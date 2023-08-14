78d113379d4e3bfaeea210323bf3cd33315396d1
OLADISEA <ladisea55@gmail.com>
2023-08-14 12:25:36 +0100

    This project includes files for bloc http request which includes app_state which shows the initial states such as the loading, loaded and error states. it also include the app_events, app bloc and the home screen.

---
+++ lib/blocs/app_blocs.dart
@@ -0,0 +1,21 @@
+import 'package:bloc_http_request/blocs/app_events.dart';
+import 'package:bloc_http_request/blocs/app_states.dart';
+import 'package:bloc_http_request/repository/repositories.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState>{
+  final UserRepository _userRepository;
+
+  UserBloc(this._userRepository): super(UserLoadingState()){
+    on<LoadUserEvent>((event, emit) async{
+      emit(UserLoadingState());
+      try{
+        final users = await _userRepository.getUsers();
+        emit(UserLoadedState(users));
+      }catch(e){
+        emit(UserErrorState(e.toString()));
+      }
+
+    });
+  }
+}
